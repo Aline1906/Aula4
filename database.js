d3b6e9827140d60614df6fcc97034e66c5a6cfa9
Aline Silva <line_adri@hotmail.com>
2021-06-03 21:03:44 -0300

    Adicionando a base de dados

---
+++ database.js
@@ -0,0 +1,46 @@
+const livros = [
+    {
+        'nome':"A Hora da Estrela",
+        'categoria':"Romance",
+        'paginas': 87,
+        'recomenda': true ,
+        'leu': true
+    },
+    {
+        'nome':"O Cortiço",
+        'categoria':"Romance",
+        'paginas': 354,
+        'recomenda': true,
+        'leu': true
+    },
+    {
+        'nome':"Alice no pais das Maravilhas",
+        'categoria':"Fantasia",
+        'paginas':224 ,
+        'recomenda':false,
+        'leu':false
+    },
+    {
+        'nome':"As cronicas de Narnia",
+        'categoria': "Fantasia",
+        'paginas': 752,
+        'recomenda':true,
+        'leu':true
+    },
+    {
+        'nome':"On the Road",
+        'categoria': "Romance",
+        'paginas': 341,
+        'recomenda': true,
+        'leu': true
+    },
+    {
+        'nome':"Star Wars: O caminho Jedi",
+        'categoria': "Fantasia",
+        'paginas': 160,
+        'recomenda': false,
+        'leu': false
+    }
+]
+
+module.exports = livros
